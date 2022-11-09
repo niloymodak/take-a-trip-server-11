100df79b727814f7d72a60e4db6fb2caf9a2e038
niloymodak <niloymodak5@gmail.com>
2022-11-09 22:22:48 +0600

    three commit

--- index.js
+++ index.js
@@ -82,18 +82,18 @@ async function run() {
             res.send(result);
         });
 
-        app.patch('/reviews/:id', verifyJWT, async (req, res) => {
-            const id = req.params.id;
-            const status = req.body.status
-            const query = { _id: ObjectId(id) }
-            const updatedDoc = {
-                $set: {
-                    status: status
-                }
-            }
-            const result = await reviewCollection.updateOne(query, updatedDoc);
-            res.send(result);
-        })
+        // app.patch('/reviews/:id', verifyJWT, async (req, res) => {
+        //     const id = req.params.id;
+        //     const status = req.body.status
+        //     const query = { _id: ObjectId(id) }
+        //     const updatedDoc = {
+        //         $set: {
+        //             status: status
+        //         }
+        //     }
+        //     const result = await reviewCollection.updateOne(query, updatedDoc);
+        //     res.send(result);
+        // })
 
         app.delete('/reviews/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
